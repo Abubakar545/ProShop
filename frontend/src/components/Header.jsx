7160cd744149f087e1534d99a9fcc7a32bef81a6
Syed <syed@git.com>
2023-10-29 15:31:02 +0530

    Frontend complete

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Navbar, Nav, Container, NavbarCollapse} from 'react-bootstrap';
+import { FaShoppingCart, FaUser} from 'react-icons/fa';
+import {LinkContainer} from 'react-router-bootstrap';
+import logo from '../assets/logo.png'
+
+const Header = () => {
+  return (
+    <header>
+         <Navbar bg="success" variant='dark' expand="lg" collapseOnSelect>
+           <Container>
+            <LinkContainer to='/'>
+              <Navbar.Brand>
+                <img src={logo} alt="Image logo"/>
+                ProShop
+                </Navbar.Brand>
+            </LinkContainer>
+              <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+              <NavbarCollapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                  <LinkContainer to="/cart">
+                    <Nav.Link>
+                      <FaShoppingCart />
+                       Cart
+                    </Nav.Link>
+                    </LinkContainer>
+                  <LinkContainer  to="/login">
+                    <Nav.Link>
+                      <FaUser />
+                       Sign In
+                    </Nav.Link>
+                  </LinkContainer>
+                </Nav>
+              </NavbarCollapse>
+           </Container>
+         </Navbar>
+    </header>
+  )
+}
+
+export default Header
